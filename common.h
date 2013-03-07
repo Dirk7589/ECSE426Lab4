892dd158f0575d01d4d746e713f492683bf25d7b
Dirk Dubois <dirk.dubois@mail.mcgill.ca>
2013-03-06 23:50:54 -0500

    Added include and extern decleration of semaphore IDs

--- common.h
+++ common.h
@@ -8,9 +8,13 @@
 #ifndef __COMMON_H
 #define __COMMON_H
 
+#include "cmsis_os.h"
+
 /*Global Variables*/
 extern float temperature; /**<The temperature variable*/
 extern float accCorrectedValues[3]; /**<The corrected accelerometer values*/
 extern float angles[2]; /**<A variable containing the pitch and roll */
 
+extern osSemaphoreId tempId;
+extern osSemaphoreId accId;
 #endif
