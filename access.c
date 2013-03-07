6d42504e56ea97cc71b97914ea9fc12bda356c1c
Dirk Dubois <dirk.dubois@mail.mcgill.ca>
2013-03-06 22:41:38 -0500

    Initial commit

---
+++ access.c
@@ -0,0 +1,14 @@
+/**
+*@file access.c
+*@author Dirk Dubois, Alain Slak
+*@date March 6th, 2013
+*@brief 
+*
+*/
+
+/**
+*@brief A function that safely access's the corrected values of the accelerometer
+*@param[inout] accValues A pointer to the new location in memory that data is copied to
+*@retval None
+*/
+void getACCValues(float* accValues);
---
+++ access.h
@@ -0,0 +1,26 @@
+/**
+*@file access.h
+*@author Dirk Dubois, Alain Slak
+*@date March 6th, 2013
+*@brief 
+*
+*/
+
+#ifndef __ACCESS_H
+#define __ACCESS_H
+
+/**
+*@brief A function that safely access's the corrected values of the accelerometer
+*@param[inout] accValues A pointer to the new location in memory that data is copied to
+*@retval None
+*/
+void getACCValues(float* accValues);
+
+/**
+*@brief A function that safely access's the temperature value of the temperature sensor
+*@param[inout] temperature A pointer to the new location in memory that data is copied to
+*@retval None
+*/
+void getTemperature(float* temperature);
+
+#endif
