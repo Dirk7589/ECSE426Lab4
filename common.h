8310b805d48ef590fd8c2f58ded3464aa19cd24a
Dirk Dubois <dirk.dubois@mail.mcgill.ca>
2013-03-06 23:04:19 -0500

    Initial commit

---
+++ common.h
@@ -0,0 +1,16 @@
+/**
+*@file common.h
+*@author Dirk Dubois, Alain Slak
+*@date March 6th, 2013
+*@brief A header file that contains the global variables being used
+*/
+
+#ifndef __COMMON_H
+#define __COMMON_H
+
+/*Global Variables*/
+extern float temperature; /**<The temperature variable*/
+extern float accCorrectedValues[3]; /**<The corrected accelerometer values*/
+extern float angles[2]; /**<A variable containing the pitch and roll */
+
+#endif
